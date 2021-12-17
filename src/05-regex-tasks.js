ae444749ccbd102ff42017341780d2a6b6e9b22d
Vladimir391 <vova.yarosh.01@inbox.ru>
2021-12-17 23:46:25 +0300

    5 задание

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -30,8 +30,8 @@
  *
  * @return {RegExp}
  */
-function getRegexForGuid() {
-  throw new Error('Not implemented');
+ function getRegexForGuid() {
+  return /{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}/;
 }
 
 /**
@@ -51,8 +51,8 @@ function getRegexForGuid() {
  * @return {RegExp}
  *
  */
-function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+ function getRegexForPitSpot() {
+  return /p.t/;
 }
 
 /**
@@ -75,8 +75,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+ function getPasswordValidator(minLength) {
+  return new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{${minLength},}$`, '');
 }
 
 module.exports = {
